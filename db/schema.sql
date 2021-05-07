0d0a6070838e520fac49dae8967fa8f4ea44082d
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-05-07 12:39:57 -0400

    added and tables and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,6 @@
-
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
 
 CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -8,16 +10,15 @@ CREATE TABLE department (
 CREATE TABLE role (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL (4, 2)
-  CONSTRAINT department_id FOREIGN KEY [departmentID] (id) REFERENCES (department)
+  salary DECIMAL (4, 2),
+  department_id INTEGER,
+  CONSTRAINT fk_department_id FOREIGN KEY (department_id) REFERENCES department(id)
 );
-
-
 
 CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  CONSTRAINT role_id  FOREIGN KEY [roleID] (id) REFERENCES (role),
-  CONSTRAINT manager_id  FOREIGN KEY [mangerID] (id) REFERENCES (role) ON UPDATE CASCADE
+  role_id INTEGER,
+  CONSTRAINT fk_role_id  FOREIGN KEY (role_id) REFERENCES role(id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+
+
+
+
+INSERT INTO employee (first_name, last_name)
+VALUES
+('Anita', 'Room'),
+('Benny', 'Factor'),
+('Jack', 'Pott'),
+('Iona', 'Mink'),
+('Don', 'Keigh');
+
+INSERT INTO role (title, salary)
+VALUES
+('Manager', 80.00),
+('Developer', 60.00),
+('Sales', 50.00);
+
+INSERT INTO department (name)
+VALUES
+('Mangement'),
+('IT'),
+('Client Acquisition');
